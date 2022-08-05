906d7640a1bf4819aa0b4fd4eb768b638dda6a14
sulfranc <sulfranc@gmail.com>
2022-08-05 15:12:18 -0400

    done

---
+++ my-app/src/components/CreateJoke.js
@@ -0,0 +1,11 @@
+function CreateJoke(){
+    return(
+        <>
+        <h2>CreateJoke</h2>
+        
+        </>
+        
+    )
+}
+
+export default CreateJoke
---
+++ my-app/src/components/RandomJoke.js
@@ -0,0 +1,40 @@
+import { useState,useEffect } from "react";
+import Joke from "./Joke";
+
+function RandomJoke() {
+    const [jokes,setJokes] = useState([])
+    const [jokeElem,setJokeElem] = useState([])
+    
+    useEffect(()=>{
+        fetch("http://localhost:3000/Jokes")
+            .then(resp => resp.json())
+            .then(data => setJokes(data))            
+            .catch(console.error())
+
+            
+
+    },[]
+    )
+        
+        function getRandomJoke(){
+            
+            console.log(jokes)
+            setJokeElem(jokes[Math.floor(Math.random() * jokes.length)])
+            console.log(jokeElem.id)
+            console.log(jokeElem.joke)
+           
+            
+       }
+    return (
+        <>
+            <h2>RandomJoke</h2>
+            <button onClick={getRandomJoke}>Get Random Joke</button>
+            <h3>{jokeElem.joke}</h3>             
+            
+
+        </>
+    )
+    
+}
+
+export default RandomJoke
