d159e19a5a0508b23cf87c46735f2edfb32e1491
sulfranc <sulfranc@gmail.com>
2022-08-05 15:34:25 -0400

    done

--- my-app/src/components/RandomJoke.js
+++ my-app/src/components/RandomJoke.js
@@ -4,6 +4,7 @@ import { useState,useEffect } from "react";
 function RandomJoke() {
     const [jokes,setJokes] = useState([])
     const [jokeElem,setJokeElem] = useState([])
+    const [showText, setShowText] = useState(false)
     
     useEffect(()=>{
         fetch("http://localhost:3000/Jokes")
@@ -22,6 +23,7 @@ function RandomJoke() {
             setJokeElem(jokes[Math.floor(Math.random() * jokes.length)])
             console.log(jokeElem.id)
             console.log(jokeElem.joke)
+            console.log(jokeElem.answer)
            
             
        }
@@ -29,7 +31,9 @@ function RandomJoke() {
         <>
             <h2>RandomJoke</h2>
             <button onClick={getRandomJoke}>Get Random Joke</button>
-            <h3>{jokeElem.joke}</h3>             
+            <h3>{jokeElem.joke}</h3>
+            <button onClick={() => setShowText(!showText)}>Get Answer</button> 
+            {showText && <h3>{jokeElem.answer}</h3>}            
             
 
         </>
